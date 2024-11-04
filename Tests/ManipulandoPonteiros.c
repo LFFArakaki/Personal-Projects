e78b2879cf87dcc4db4b6e1471e8fc7f2b99f44b
Luis Fellipe <luisfellipe.arakaki@gmail.com.br>
2024-11-04 11:42:53 -0300

    UpdatePonteiros

--- Tests/ManipulandoPonteiros.c
+++ Tests/ManipulandoPonteiros.c
@@ -3,30 +3,31 @@
 #include <stdlib.h>
 
 #define CARACTERES 100
-#define MAXPRODUTOS 500
+#define MAXPRODUTOS 3
 
+int ch;
 struct produto {
         int codigo;
 		char nome[CARACTERES];
 		int quantidade;
-		double preco;
+		float preco;
 	};
 	
 int menuOpcoes();
-void incluirProduto(struct produto *ponteiroProdutos);
-void alterarProduto(struct produto *ponteiroProdutos);
-void excluirProduto(struct produto *ponteiroProdutos);
-void comprarProduto(struct produto *ponteiroProdutos);
-void consultarProduto(struct produto *ponteiroProdutos);
-void listarProdutos(struct produto *ponteiroProdutos);
+void incluirProduto(struct produto *ponteiroProdutos, int *ponteiroNumeroDeProdutos);
+void alterarProduto(struct produto *ponteiroProdutos, int *ponteiroNumeroDeProdutos);
+void excluirProduto(struct produto *ponteiroProdutos, int *ponteiroNumeroDeProdutos);
+void comprarProduto(struct produto *ponteiroProdutos, int *ponteiroNumeroDeProdutos);
+void consultarProduto(struct produto *ponteiroProdutos, int *ponteiroNumeroDeProdutos);
+void listarProdutos(struct produto *ponteiroProdutos, int *ponteiroNumeroDeProdutos);
 void mudarProdutos(struct produto *ponteiroProdutos, int i);
 
 int main()
 {
-    int loop;
+    int loop, numeroDeProdutos;
+    int *ponteiroNumeroDeProdutos = &numeroDeProdutos;
     struct produto produtos[MAXPRODUTOS];
     struct produto *ponteiroProdutos = produtos;
-    loop=0;
     
     while(loop == 0)
     {
@@ -38,22 +39,28 @@ int main()
             return 0;
             break;
             case 1:
-            incluirProduto(ponteiroProdutos);
+            if(numeroDeProdutos == MAXPRODUTOS)
+            {
+                printf("Não é possível cadastrar mais produtos no momento!\n");
+                printf("Exclua um produto e tente novamente!\n");
+                break;
+            }
+            incluirProduto(ponteiroProdutos, ponteiroNumeroDeProdutos);
             break;
             case 2:
-            alterarProduto(ponteiroProdutos);
+            alterarProduto(ponteiroProdutos, ponteiroNumeroDeProdutos);
             break;
             case 3:
-            excluirProduto(ponteiroProdutos);
+            excluirProduto(ponteiroProdutos, ponteiroNumeroDeProdutos);
             break;
             case 4:
-            comprarProduto(ponteiroProdutos);
+            comprarProduto(ponteiroProdutos, ponteiroNumeroDeProdutos);
             break;
             case 5:
-            consultarProduto(ponteiroProdutos);
+            consultarProduto(ponteiroProdutos, ponteiroNumeroDeProdutos);
             break;
             case 6:
-            listarProdutos(ponteiroProdutos);
+            listarProdutos(ponteiroProdutos, ponteiroNumeroDeProdutos);
             break;
         }
     }
@@ -75,6 +82,7 @@ int menuOpcoes()
     printf("6 - Ver lista de produtos\n");
     
     scanf("%i",&resposta);
+    while((ch = getchar()) != '\n' && ch != EOF);
     
     if(resposta < 0 || resposta > 6)
     {
@@ -88,38 +96,49 @@ int menuOpcoes()
     }
 }
 
-void incluirProduto(struct produto *ponteiroProdutos)
+void incluirProduto(struct produto *ponteiroProdutos, int *ponteiroNumeroDeProdutos)
+{
+    (ponteiroProdutos + *ponteiroNumeroDeProdutos)->codigo = *ponteiroNumeroDeProdutos+1;
+    printf("Qual o nome do produto %i?\n",*ponteiroNumeroDeProdutos+1);
+    fgets((ponteiroProdutos + *ponteiroNumeroDeProdutos)->nome, 100, stdin);
+    printf("Qual o preço desse produto?\n");
+    scanf("%f",&(ponteiroProdutos + *ponteiroNumeroDeProdutos)->preco);
+    printf("Quantos desse produto tem em estoque?\n");
+    scanf("%i",&(ponteiroProdutos + *ponteiroNumeroDeProdutos)->quantidade);
+    while((ch = getchar()) != '\n' && ch != EOF);
+    
+    *ponteiroNumeroDeProdutos = *ponteiroNumeroDeProdutos+1;
+    
+    return;
+}
+
+void alterarProduto(struct produto *ponteiroProdutos, int *ponteiroNumeroDeProdutos)
 {
     
 }
 
-void alterarProduto(struct produto *ponteiroProdutos)
+void excluirProduto(struct produto *ponteiroProdutos, int *ponteiroNumeroDeProdutos)
 {
     
 }
 
-void excluirProduto(struct produto *ponteiroProdutos)
+void comprarProduto(struct produto *ponteiroProdutos, int *ponteiroNumeroDeProdutos)
 {
     
 }
 
-void comprarProduto(struct produto *ponteiroProdutos)
+void consultarProduto(struct produto *ponteiroProdutos, int *ponteiroNumeroDeProdutos)
 {
-    
+
 }
 
-void consultarProduto(struct produto *ponteiroProdutos)
-{
- 
-}
-
-void listarProdutos(struct produto *ponteiroProdutos)
+void listarProdutos(struct produto *ponteiroProdutos, int *ponteiroNumeroDeProdutos)
 {
     printf("\n----------------------------------------------------\n");
     printf("|                TABELA DE PRODUTOS                |\n");
     printf("----------------------------------------------------\n");
     printf("|  ID  |        NOME        |   PREÇO   |QUANTIDADE|\n");
-    for(int i=0;i<MAXPRODUTOS;i++)
+    for(int i=0;i<*ponteiroNumeroDeProdutos;i++)
     {
         (ponteiroProdutos + i)->nome[strcspn((ponteiroProdutos + i)->nome, "\n")] = '\0';
         printf("----------------------------------------------------\n");   
